c90bb3e47cecb3e076465e8bb3456001df86944d
DarkBrave_ <43324814+DarkBrave@users.noreply.github.com>
2020-09-04 10:05:13 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hi! I'm DarkBrave_!");
+    System.out.println(" ");
+    System.out.println("This is my GitHub profile, where I post most of my code. I also have a YouTube channel (DarkBrave_) where I post videos. I am currently learning to code in Java, and I already know basic HTML and CSS.");
+  }
+}
